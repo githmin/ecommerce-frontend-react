998d39fa068f0e96f2179e227ef05e8091af89e5
Githmin Jayawardhana <githmin21@gmail.com>
2022-11-12 22:17:17 +0530

    added announcements bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Announcements } from '../components/Announcements'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
-    <div><Navbar/></div>
+    <div>
+      <Announcements/>
+      <Navbar/>
+      </div>
   )
 }
 
